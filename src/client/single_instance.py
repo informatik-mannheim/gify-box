f3dbb3ac7eb47d9c7faed50cb583b87feed126b2
Thomas Smits <thomsmits@gmail.com>
2022-04-14 15:57:09 +0200

    Some minor adjustments
    Adjustments to the code layout and added some comments.

--- src/client/single_instance.py
+++ src/client/single_instance.py
@@ -3,90 +3,108 @@
 """
 Main program for the Gify-Box.
 
+This program is currently running with Python 2.
+
 https://github.com/informatik-mannheim/gify-box
 """
 
 from time import sleep
 from gpiozero import Button
-import os, random, picamera, requests, serial
+import os, random, picamera, requests
 from neopixel import *
 from subprocess import Popen
-import adafruit_thermal as at
+import sys, select
+import subprocess
+
 
 ### !! VAR DEFINITIONS !! ###
 
-
 # LED strip configuration. #1 is at button, #2 at the camera
-LED_COUNT = 8  # Number of LED pixels.
-LED_PIN = 18  # GPIO pin connected to the button pixels (must support PWM!).
-LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 5  # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 50  # Set to 0 for darkest and 255 for brightest
-LED_BRIGHTFAC = 0.2  # factor of the default brightness
-LED_INVERT = False  # True to invert the signal (when using NPN transistor)
+LED_COUNT          = 8       # Number of LED pixels.
+LED_PIN            = 18      # GPIO pin connected to the button pixels (must support PWM!).
+LED_FREQ_HZ        = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA            = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS     = 50      # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTFAC      = 0.2     # factor of the default brightness
+LED_INVERT         = False   # True to invert the signal (when using NPN transistor)
 
 # GPIO pins according to BCM (http://pinout.xyz)
 PINBTN = 23
 
+
+
 # Picture configuration
-PICTURE_COUNT = 5
-RESOLUTION = (1280, 720)
+PICTURE_COUNT   = 5
+RESOLUTION      = (1280, 720)
+
+
 
 # Picture wait delays
-COMPLIMENT_WAIT = 0.8  # seconds
-REPLAY_WAIT = 24  # seconds
-GOODBYE_WAIT = 6  # seconds
-STARTING_WAIT = 2500 // LED_COUNT  # if we wait that amount after each LED, the whole process takes 1 second
-PHOTOSHOOT_WAIT = 360 // LED_COUNT  # time between photos
-GIF_DELAY = 35  # How much time (1/100th seconds) between frames in the animated gif
+COMPLIMENT_WAIT = 0.8 # seconds
+REPLAY_WAIT     = 24 # seconds
+GOODBYE_WAIT    = 6 # seconds
+STARTING_WAIT   = 1200/LED_COUNT    # if we wait that amount after each LED, the whole process takes 1 second
+PHOTOSHOOT_WAIT = 240/LED_COUNT        # time between photos
+GIF_DELAY       = 35 # How much time (1/100th seconds) between frames in the animated gif
+
+
 
 # Color values (CAUTION: NOT RGB but GRB: Green, Red, Blue)
-COLOR_OK = Color(255, 0, 0)  # GREEN
-COLOR_BLACK = Color(0, 0, 0)  # BLACK
-COLOR_INITCOUNTDOWN1 = Color(150, 0, 0)
-COLOR_INITCOUNTDOWN2 = Color(30, 0, 0)
-COLOR_INITCOUNTDOWN3 = Color(10, 0, 0)
-COLOR_INITCOUNTDOWN4 = Color(5, 0, 0)
-COLOR_IMAGECOUNTDOWN = Color(255, 255, 255)
-COLOR_GIFGENERATION = Color(0, 0, 255)
+COLOR_OK                = Color(255, 0, 0)    #GREEN
+COLOR_BLACK             = Color(0, 0, 0) # BLACK
+COLOR_INITCOUNTDOWN1    = Color(150, 0, 0)
+COLOR_INITCOUNTDOWN2    = Color(30, 0, 0)
+COLOR_INITCOUNTDOWN3    = Color(10, 0, 0)
+COLOR_INITCOUNTDOWN4    = Color(5, 0, 0)
+COLOR_IMAGECOUNTDOWN    = Color(255, 255, 255)
+COLOR_GIFGENERATION     = Color(0, 0, 255)
 COLOR_GIFGENERATIONDARK = Color(0, 0, 120)
-COLOR_REPLAY = Color(0, 255, 0)
+COLOR_REPLAY            = Color(0, 255, 0)
+
+
+
 
 # Paths
-PATH_FILEPATH = '/home/pi/photobooth/'
-PATH_DATAFILE = PATH_FILEPATH + 'count.txt'
-PATH_OUTPUT = PATH_FILEPATH + 'output/'
-PATH_OUTPUTROUND = PATH_OUTPUT + 'round%06d/'
-PATH_OUTPUTFILE = PATH_OUTPUTROUND + 'frame%02d.jpg'
+PATH_FILEPATH      = '/home/pi/photobooth/'
+PATH_DATAFILE      = PATH_FILEPATH + 'count.txt'
+PATH_OUTPUT        = PATH_FILEPATH + 'output/'
+PATH_OUTPUTROUND   = PATH_OUTPUT + 'round%06d/'
+PATH_OUTPUTFILE    = PATH_OUTPUTROUND + 'frame%02d.jpg'
 PATH_OUTPUTFILEGIF = PATH_OUTPUT + 'round%06d.gif'
 
 # Branding logo overlay
-OVERLAYIMAGE_SRC = PATH_FILEPATH + 'media/logo_makerfaire.png'
-OVERLAYIMAGE_OFFSET = (50, 30)
+OVERLAYIMAGE_SRC    = PATH_FILEPATH + 'media/logo_innospace.png'
+OVERLAYIMAGE_OFFSET = (30, 10)
+
+
 
 # Camera text annotations
 CAMERA_TEXTCOLOR = picamera.Color('white')
 CAMERA_TEXTBACKGROUNDCOLOR = picamera.Color('black')
 
-CAMERA_TEXTVAL_START = 'Get %d poses ready & press the button to start' % PICTURE_COUNT
-CAMERA_TEXTVAL_STARTING1 = 'Animated GIF-Box is starting. Session #%06d'
-CAMERA_TEXTVAL_STARTING2 = 'It will take %d pictures' % PICTURE_COUNT
-CAMERA_TEXTVAL_STARTING3 = 'The LED strip will fill towards the camera'
-CAMERA_TEXTVAL_STARTING4 = 'When it reaches the camera, a photo is taken'
-CAMERA_TEXTVAL_STARTING5 = 'Get ready! We are launching.'
+CAMERA_TEXTVAL_START          = 'Get %d poses ready & press the button'%PICTURE_COUNT
+CAMERA_TEXTVAL_STARTING1      = 'Taking Gif #%06d'
+CAMERA_TEXTVAL_STARTING2      = 'Let`s go!'
+CAMERA_TEXTVAL_STARTING3      = '3...'
+CAMERA_TEXTVAL_STARTING4      = '2...'
+CAMERA_TEXTVAL_STARTING5      = '1...'
 CAMERA_TEXTVAL_PICINFORMATION = 'Taking picture #%d'
-CAMERA_TEXTVAL_PROCESSING = 'Stitching your photos together. Please wait a few seconds.'
+CAMERA_TEXTVAL_PROCESSING     = 'Stitching your photos together. Please wait :)'
 CAMERA_TEXTVAL_PROCESSINGDONE = 'Almost there'
-CAMERA_TEXTVAL_GOODBYE = 'It was great having you here! See you around.'
+CAMERA_TEXTVAL_GOODBYE        = 'It was great having you here!'
 
 # List of compliments displayed after each photo. Needs atleast as many entries as the picture count!!!
-CAMERA_TEXTVAL_COMPLIMENTS = ['Looking good!', 'Oh yeah!', 'You rock!', 'Just like that!', 'Keep it up!', 'Yes!',
-                              'Great!', 'Oh wow!', 'Perfect!', 'Nice!']
+CAMERA_TEXTVAL_COMPLIMENTS = ['Looking good!', 'Oh yeah!', 'You rock!', 'Just like that!', 'Keep it up!', 'Yes!', 'Great!', 'Oh wow!', 'Perfect!', 'Nice!']
+
 
 # webserver data
 WEBSERVER_URL = 'http://gifybox.inno-space.de/upload.php'
 
+
+
 ### !! DEFINITIONS DONE !! ###
+
+
 
 ### !! NEO PIXEL ANIMATIONS - SEE https://github.com/jgarff/rpi_ws281x !! ###
 
@@ -98,22 +116,16 @@ strip.begin()
 def color_wipe(strip_to_use, color, wait_ms=20, reverse=False):
     """
     Perform a color wipe using the attached neopixels.
-
-    :param strip_to_use: the strip to be used
-    :param color: the color to set
-    :param wait_ms: time (in milliseconds) to wait between steps
-    :param reverse: reverse the effect (True) or not (False)
     """
 
     my_range = range(LED_COUNT) if not reverse else reversed(range(LED_COUNT))
 
     # wipe color: do a color wipe with this color
     for i in my_range:
-        strip_to_use.setPixelColor(i, color)
-        strip_to_use.show()
+        strip.setPixelColor(i, color)
+        strip.show()
         if wait_ms > 0:
-            sleep(wait_ms / 1000.0)
-
+            sleep(wait_ms/1000.0)
 
 # Show color instantaneous w/o any animation
 def color_clear(strip_to_use, color):
@@ -148,13 +160,10 @@ def camera_print_text(camera_to_use, text):
 
 ### !! BUSINESS LOGIC START !! ###
 
-# connect to the printer with default port and speed
-printer = at.AdafruitThermal()
-printer.begin()
-
 # start the camera
 camera = picamera.PiCamera()
 camera.resolution = RESOLUTION
+#camera.rotation = 180
 
 # turn off that red camera led
 camera.led = False
@@ -170,6 +179,9 @@ camera_print_text(camera, CAMERA_TEXTVAL_START)
 # get the hardware button
 button = Button(PINBTN)
 
+# start the serial connection
+#ser = serial.Serial(port='/dev/ttyACM0', baudrate=19200)
+
 # get the data file and read the current round from there so we dont overwrite stuff
 mround = 0
 try:
@@ -179,16 +191,19 @@ try:
 except:
     print("File content error")
 
+
+
 while True:
+
     # try to create a folder (don't abort if already there)
     try:
-        os.mkdir(PATH_OUTPUTROUND % mround, 0777)
+        os.mkdir(PATH_OUTPUTROUND%mround, 0o777)
     except OSError:
         print("dir already there")
 
     # wait for the button press
     button.wait_for_press()
-    camera_print_text(camera, CAMERA_TEXTVAL_STARTING1 % mround)
+    camera_print_text(camera, CAMERA_TEXTVAL_STARTING1%mround)
 
     # do start animation
     color_wipe(strip, COLOR_INITCOUNTDOWN1, wait_ms=STARTING_WAIT)
@@ -210,19 +225,18 @@ while True:
     # loop through the pictures
     while frame < PICTURE_COUNT:
         # show photo number and start light animation
-        camera_print_text(camera, CAMERA_TEXTVAL_PICINFORMATION % (frame + 1))
+        camera_print_text(camera, CAMERA_TEXTVAL_PICINFORMATION%(frame+1))
         color_wipe(strip, COLOR_IMAGECOUNTDOWN, wait_ms=PHOTOSHOOT_WAIT, reverse=True)
 
         # clear the text and take a picture
         camera_print_text(camera, False)
-        filepath = PATH_OUTPUTFILE % (mround, frame)
+        filepath = PATH_OUTPUTFILE%(mround,frame)
         camera.capture(filepath, use_video_port=True)
 
         # add branding and scale down the image using graphicsmagick
-        graphicsmagick = "gm composite "
-        graphicsmagick += "-gravity SouthEast -geometry +" + str(OVERLAYIMAGE_OFFSET[0]) + "+" + str(
-            OVERLAYIMAGE_OFFSET[0]) + " "  # bottom right with padding
-        graphicsmagick += OVERLAYIMAGE_SRC + " " + filepath + " " + filepath  # overlay image, source image, target image
+        graphicsmagick  = "gm composite "
+        graphicsmagick += "-gravity SouthEast -geometry +" + str(OVERLAYIMAGE_OFFSET[0]) + "+" + str(OVERLAYIMAGE_OFFSET[1]) + " "    # bottom right with padding
+        graphicsmagick += OVERLAYIMAGE_SRC + " " + filepath + " " + filepath # overlay image, source image, target image
         os.system(graphicsmagick)
 
         # clear the lights
@@ -239,25 +253,35 @@ while True:
     color_wipe(strip, COLOR_GIFGENERATION)
 
     # upload pictures to server
-    images = [('image%d' % x, open(PATH_OUTPUTFILE % (mround, x), 'rb')) for x in range(PICTURE_COUNT)]
-    r = requests.post(WEBSERVER_URL, files=images)
+    images = [('image%d'%x, open(PATH_OUTPUTFILE%(mround,x), 'rb')) for x in range(PICTURE_COUNT)]
+    r = requests.post(WEBSERVER_URL, files = images)
+    with open("randomfile.txt","a") as o:
+        o.write(str(r.status_code))
+
     print("upload finished with result code: %s" % str(r.status_code))
 
     if r.status_code == 200:
         filename = r.text
         print("printing user receipt with URL: %s" % str(r.text))
-        printer.wake()
-        printer.println("inno.space proudly presents")
-        printer.print_qr_code(r.text)
-        printer.sleep()
+        # Print the QR code using the attached
+        # Adafruit Thermal printer
+        # Due to the fact that the printer driver and the
+        # program to print the QR code are written in
+        # Python 3 but we are currently running under
+        # Python 2, the printing is delegated to an external
+        # process. As soon as we are ported to Python 3
+        # the skript should be imported and simply called
+        # inline
+        subprocess.call(['/home/pi/photobooth/print_qr.py', r.text])
+
 
     # create the gif
-    graphicsmagick = "gm convert -delay " + str(
-        GIF_DELAY) + " " + PATH_OUTPUTROUND % mround + "*.jpg " + PATH_OUTPUTFILEGIF % mround
-    os.system(graphicsmagick)  # make the .gif
+    graphicsmagick = "gm convert -delay " + str(GIF_DELAY) + " " + PATH_OUTPUTROUND%mround + "*.jpg " + PATH_OUTPUTFILEGIF%mround
+    os.system(graphicsmagick) #make the .gif
 
     # start the gif viewer
-    command = ["viewnior", "--fullscreen", PATH_OUTPUTFILEGIF % mround]
+    # TODO: This command tends to crash with a "cannot open display"
+    command = ["viewnior", "--fullscreen", PATH_OUTPUTFILEGIF%mround]
     p = Popen(command)
 
     # display the text telling it is almost done, since we still need time to start the replay
@@ -290,5 +314,10 @@ while True:
     f = open("count.txt", "w")
     f.write(str(mround))
     f.close()
+
+    # Allow quitting by pressing a key
+    #a, b, c = select.select( [sys.stdin], [],[],1)
+    #if (a != []):
+    #   sys.exit(0)
 
 ### !! BUSINESS LOGIC DONE !! ###
