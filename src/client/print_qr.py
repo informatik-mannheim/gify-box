677b298c220afa80f9e26013ff42f7932c4c39d9
Thomas Smits <thomsmits@gmail.com>
2022-04-14 15:56:35 +0200

    Better slip
    Improved the look of the printed slip.

--- src/client/print_qr.py
+++ src/client/print_qr.py
@@ -23,14 +23,23 @@ def print_qr_code(text):
     printer.begin()
     printer.justify('C')
 
+    printer.println()
     printer.print_image(logo)
+    printer.set_size('S')
+    printer.println("https://inno-space.de")
     printer.set_size('M')
     printer.println("proudly presents")
     printer.set_size('L')
     printer.println("Gify Box")
-
+    printer.println()
+    printer.set_size('S')
+    printer.println("Download your picture here:")
     printer.print_qr_code(text)
     printer.println(text)
+    printer.println()
+    printer.println()
+    printer.println()
+    printer.println()
 
 if __name__ == "__main__":
     print_qr_code(sys.argv[1])
